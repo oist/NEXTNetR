187ab1859e205aaed6194b645ed6ff9cdf696cb0
Florian G. Pflug <fgp@phlo.org>
2025-04-25 17:54:47 +0900

    Use safe[Rf_warning](...) instead of warning(...) since the latter crashes.
    The difference is that warning() calls Rf_warningcall instead of Rf_warning.
    Why that fails when Rf_warning works is unclear.

--- src/options.h
+++ src/options.h
@@ -114,7 +114,7 @@ list process_options(const list& opts, Handlers... handlers)
 
     // Warn about unprocessed options
     for(const auto& i: opts_unprocessed)
-        warning("ignoring unknown option %s", i.first.c_str());
+        safe[Rf_warning]("ignoring unknown option %s", i.first.c_str());
 
     return opts_out_list;
 }

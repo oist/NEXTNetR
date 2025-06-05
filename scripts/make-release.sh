4fb90e2ae71f17c4b51d6d487f34bec7a595175d
Florian G. Pflug <fgp@phlo.org>
2025-06-05 23:23:24 +0900

    Improved scripts/make-release.sh

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -21,12 +21,40 @@ rm -f  archives/NEXTNetR-v$ver-full.tar.gz
 ./scripts/git-archive-all.sh --tree-ish v$ver --prefix NEXTNetR/ archives/NEXTNetR-v$ver-full.tar
 gzip --best archives/NEXTNetR-v$ver-full.tar
 
-echo "Extracting archive to .buildpkg/" >&2
+echo "Building source archive for version $ver" >&2
 rm -rf .buildpkg
 mkdir .buildpkg
-(cd .buildpkg; tar xzf ../archives/NEXTNetR-v$ver-full.tar.gz)
+cd .buildpkg
+tar xzf ../archives/NEXTNetR-v$ver-full.tar.gz
+tar czf ../archives/NEXTNetR-v$ver-src.tar.gz \
+	NEXTNetR/DESCRIPTION \
+	NEXTNetR/NAMESPACE \
+	NEXTNetR/configure \
+	NEXTNetR/src/Makevars* \
+	NEXTNetR/src/*.{cpp,h,hpp} \
+	NEXTNetR/ext/nextnet/nextnet/*.{cpp,h} \
+	NEXTNetR/ext/nextnet/nextnet/pstream/*.{cpp,h} \
+	NEXTNetR/ext/nextnet/ext/prio_queue/prio_queue.hpp \
+	NEXTNetR/ext/nextnet/ext/dyndist/dyndist/*.h \
+	NEXTNetR/R/*.R \
+	NEXTNetR/man/*.Rd \
+	NEXTNetR/examples/*.R \
+	NEXTNetR/inst/CITATION \
+	NEXTNetR/tests/** \
+	NEXTNetR/vignettes/*.{Rmd,nw} \
+	NEXTNetR/.Rbuildignore \
+	NEXTNetR/LICENSE.md \
+	NEXTNetR/README.md
+cd ../
+rm -rf .buildpkg
+
 
 echo "Building proper R package archives/NEXTNetR-v$ver-pkg.tar.gz" >&2
-R CMD build .buildpkg/NEXTNetR
-mv NEXTNetR_$ver.tar.gz archives/NEXTNetR-v$ver-pkg.tar.gz
+rm -rf .buildpkg
+mkdir .buildpkg
+cd .buildpkg
+tar xzf ../archives/NEXTNetR-v$ver-src.tar.gz
+R CMD BUILD NEXTNetR
+mv NEXTNetR_$ver.tar.gz ../archives/NEXTNetR-v$ver-pkg.tar.gz
+cd ../
 rm -r .buildpkg
